abe86f257a46ead5acc992f9baa5f5e598f0f3d6
Victor Llallico <i2211613@continental.edu.pe>
2023-05-30 19:25:12 -0500

    Elaborando la tabla Categoria a migrar

---
+++ backend/database/migrations/2023_04_25_0011_credito_categoria.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $this->down();
+        Schema::create('credito_categoria', function (Blueprint $table) {
+            $table->id();
+            $table->string('categoria', 50);
+            $table->boolean('habilitado')->default(1);
+            $table->timestamps();
+        });
+
+        $categorias = [];
+        $categorias[] = (object) array('categoria' => 'CATEGORIA_1');
+        $categorias[] = (object) array('categoria' => 'CATEGORIA_2');
+        
+        foreach ($categorias as $value) {
+            DB::table('credito_categoria')->insert([
+                'categoria' => $value->categoria
+            ]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('credito_categorias');
+        Schema::enableForeignKeyConstraints();
+    }
+};
